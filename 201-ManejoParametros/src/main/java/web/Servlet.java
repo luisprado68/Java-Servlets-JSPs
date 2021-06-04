e3c9fd4128c7780de2c4837ade25fcc2fc09a402
Luis Prado <luisprado0095@gmail.com>
2021-06-04 13:23:21 -0300

    :memo:

---
+++ 201-ManejoParametros/src/main/java/web/Servlet.java
@@ -0,0 +1,31 @@
+
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+;
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        //leer los parametros del formulario html
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+        
+        System.out.println("password = " + usuario);
+        System.out.println("password = " + password);
+        //puede dar un ioexception
+        PrintWriter out = response.getWriter();
+        out.print("<html>");
+        out.print("<body>");
+        out.print("El parametro de usuario es: " + usuario);
+        out.print("<br>");
+        out.print("El parametro de password es: " + password);
+        out.print("</body>");
+        out.print("</html>");
+        out.close();
+    }
+}
