f833a99b6fa0865c98dea7e9bde2c38df6de3545
Luis Prado <luisprado0095@gmail.com>
2021-06-08 00:46:48 -0300

    :memo:

---
+++ 202-ManejoFormularios/src/main/java/domain/Servlet.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet{
+    
+    
+}
